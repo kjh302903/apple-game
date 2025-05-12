a51064176833fe58d54a36feeeaf6e06cda55697
kjh302903 <kjh302903@gmail.com>
2025-05-12 14:44:34 +0900

    fix: 배경 최소 널이 설정
    배경 최소 넓이 설정

--- src/app/_component/BackGround.tsx
+++ src/app/_component/BackGround.tsx
@@ -2,7 +2,9 @@ interface Props {
   children: React.ReactNode;
 }
 const BackGround = ({ children }: Props) => {
-  return <div className="h-dvh pt-24 bg-[#fff8e1]">{children}</div>;
+  return (
+    <div className="h-dvh pt-24 bg-[#fff8e1]  min-w-[780px]">{children}</div>
+  );
 };
 
 export default BackGround;
